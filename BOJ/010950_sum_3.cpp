dd4aa24fbeb9301fbbfe8832ace1502eeb9810e2
sml0399 <sml0399@naver.com>
2021-01-11 00:18:34 +0900

    010950

---
+++ BOJ/010950_sum_3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(void){
+  int cases=0,a=0,b=0;
+  cin>>cases;
+  for(int i=0;i<cases;i++){
+    cin>>a>>b;
+    cout<<(a+b)<<endl;
+  }
+  return 0;
+}
