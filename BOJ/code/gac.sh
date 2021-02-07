bd0e9c7d2552dfba6fc7189e71dfb719719f9c61
sml0399 <sml0399@naver.com>
2021-02-07 23:35:06 +0900

    modified gac.sh

--- BOJ/code/gac.sh
+++ BOJ/code/gac.sh
@@ -2,3 +2,4 @@ cd ..
 read -p "commit name:" name
 git add .
 git commit -m "${name}"
+git push -u origin main
--- BOJ/gac.sh
+++ BOJ/gac.sh
@@ -1,3 +1,4 @@
 read -p "commit name:" name
 git add .
 git commit -m "${name}"
+git push -u origin main
